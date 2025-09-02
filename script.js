4737aada0859542de090b3ccd9bf509b5b0833e5
Leandro <leandroprofka@icloud.com>
2025-09-02 22:19:43 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,76 @@
+"use strict";
+
+/*
+function greet(name) {
+  console.log(`Hello, ${name}`);
+}
+
+greet("Jim");
+
+// testing undefined as a return
+function doSomething() {
+  console.log("doing something...");
+}
+
+let result = doSomething();
+console.log(result); // prints in console "undefined" as the default of return is "undefined"
+
+// returning a specific value
+function calculateSum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(calculateSum(3, 4)); // prints 7
+
+// anonymous function
+const sum = function (num1, num2) {
+  return num1 + num2;
+};
+
+console.log(sum(3, 4));
+
+// dealing with default arguments for functions
+function greetings(name = "Guest") {
+  console.log(`Hello, ${name}!`);
+}
+
+greetings(); // Hello, Guest!
+greetings("Anna"); // Hello, Anna!
+
+
+// dealing with Arrow Functions =>
+
+function greet(name) {
+  console.log(`Hello, ${name}`);
+}
+
+const greetings = (name) => {
+  console.log(`Hello, ${name}!`);
+};
+
+const calculateArea = (width, height) => width * height;
+
+console.log(calculateArea(5, 3));
+
+// global, local and block scope
+
+function greet() {
+  let message = "Hello, local scope!";
+  console.log(message);
+}
+
+greet();
+console.log(message); // this will throw an error as message is in the local scope of the function and can't be accesses in a global scope
+
+// block scope is defined by the curly braces {}, usually in if statements, for loops and while loops.
+
+if (true) {
+  let blockVar = "I'm in a block";
+  console.log(blockVar); // this will print the string
+}
+
+console.log(blockVar); // this will throw an error, however, if it was a VAR variable, it could be accessed
+
+*/
+
+function addTwoAndSeven() {}
